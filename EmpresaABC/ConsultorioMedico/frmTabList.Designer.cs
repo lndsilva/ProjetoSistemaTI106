2b2ac0b431662ab5199d8862e678dbf92daf729b
lndsilva <lndsilva@hotmail.com>
2023-08-10 13:18:16 -0300

    atualização

---
+++ EmpresaABC/ConsultorioMedico/frmTabList.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace ConsultorioMedico
+{
+    partial class frmTabList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTabList));
+            this.tbcCadastro = new System.Windows.Forms.TabControl();
+            this.tbpCadastro = new System.Windows.Forms.TabPage();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.ckbAutorizo = new System.Windows.Forms.CheckBox();
+            this.lblProgaganda = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.tbpLista = new System.Windows.Forms.TabPage();
+            this.dgvCadastro = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.autorizo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.tbcCadastro.SuspendLayout();
+            this.tbpCadastro.SuspendLayout();
+            this.tbpLista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbcCadastro
+            // 
+            this.tbcCadastro.Controls.Add(this.tbpCadastro);
+            this.tbcCadastro.Controls.Add(this.tbpLista);
+            this.tbcCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbcCadastro.Location = new System.Drawing.Point(37, 70);
+            this.tbcCadastro.Name = "tbcCadastro";
+            this.tbcCadastro.SelectedIndex = 0;
+            this.tbcCadastro.Size = new System.Drawing.Size(676, 400);
+            this.tbcCadastro.TabIndex = 0;
+            // 
+            // tbpCadastro
+            // 
+            this.tbpCadastro.Controls.Add(this.btnLimpar);
+            this.tbpCadastro.Controls.Add(this.btnCadastrar);
+            this.tbpCadastro.Controls.Add(this.ckbAutorizo);
+            this.tbpCadastro.Controls.Add(this.lblProgaganda);
+            this.tbpCadastro.Controls.Add(this.txtNome);
+            this.tbpCadastro.Controls.Add(this.txtCodigo);
+            this.tbpCadastro.Controls.Add(this.lblNome);
+            this.tbpCadastro.Controls.Add(this.lblCodigo);
+            this.tbpCadastro.Location = new System.Drawing.Point(4, 29);
+            this.tbpCadastro.Name = "tbpCadastro";
+            this.tbpCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpCadastro.Size = new System.Drawing.Size(668, 367);
+            this.tbpCadastro.TabIndex = 0;
+            this.tbpCadastro.Text = "Cadastro";
+            this.tbpCadastro.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(524, 293);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(124, 39);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(381, 293);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(124, 39);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "&Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click_1);
+            // 
+            // ckbAutorizo
+            // 
+            this.ckbAutorizo.AutoSize = true;
+            this.ckbAutorizo.Location = new System.Drawing.Point(66, 259);
+            this.ckbAutorizo.Name = "ckbAutorizo";
+            this.ckbAutorizo.Size = new System.Drawing.Size(87, 24);
+            this.ckbAutorizo.TabIndex = 8;
+            this.ckbAutorizo.Text = "Autorizo";
+            this.ckbAutorizo.UseVisualStyleBackColor = true;
+            // 
+            // lblProgaganda
+            // 
+            this.lblProgaganda.AutoSize = true;
+            this.lblProgaganda.Location = new System.Drawing.Point(62, 204);
+            this.lblProgaganda.Name = "lblProgaganda";
+            this.lblProgaganda.Size = new System.Drawing.Size(251, 20);
+            this.lblProgaganda.TabIndex = 9;
+            this.lblProgaganda.Text = "Autorizo o envio de propagandas?";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(137, 136);
+            this.txtNome.MaxLength = 100;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(285, 26);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(137, 73);
+            this.txtCodigo.MaxLength = 10;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 26);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(62, 139);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(55, 20);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(62, 76);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(63, 20);
+            this.lblCodigo.TabIndex = 5;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // tbpLista
+            // 
+            this.tbpLista.Controls.Add(this.dgvCadastro);
+            this.tbpLista.Location = new System.Drawing.Point(4, 29);
+            this.tbpLista.Name = "tbpLista";
+            this.tbpLista.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpLista.Size = new System.Drawing.Size(668, 367);
+            this.tbpLista.TabIndex = 1;
+            this.tbpLista.Text = "Lista";
+            this.tbpLista.UseVisualStyleBackColor = true;
+            // 
+            // dgvCadastro
+            // 
+            this.dgvCadastro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCadastro.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.nome,
+            this.autorizo});
+            this.dgvCadastro.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCadastro.Location = new System.Drawing.Point(3, 3);
+            this.dgvCadastro.Name = "dgvCadastro";
+            this.dgvCadastro.Size = new System.Drawing.Size(662, 361);
+            this.dgvCadastro.TabIndex = 0;
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "Código";
+            this.codigo.Name = "codigo";
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            // 
+            // autorizo
+            // 
+            this.autorizo.HeaderText = "Autorizo envio para propaganda?";
+            this.autorizo.Name = "autorizo";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(585, 498);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(124, 39);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmTabList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 564);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.tbcCadastro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTabList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.tbcCadastro.ResumeLayout(false);
+            this.tbpCadastro.ResumeLayout(false);
+            this.tbpCadastro.PerformLayout();
+            this.tbpLista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCadastro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbcCadastro;
+        private System.Windows.Forms.TabPage tbpCadastro;
+        private System.Windows.Forms.TabPage tbpLista;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.CheckBox ckbAutorizo;
+        private System.Windows.Forms.Label lblProgaganda;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.DataGridView dgvCadastro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn autorizo;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
